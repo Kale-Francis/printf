9069c91a93fe9e0a182870b07bbd2c9bbc9944be
king-vader <kalefrancis989#gamil.com>
2024-08-24 05:56:09 +0300

    Updated code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-c-printf
-updated project
+# Custom printf Implementation
+
+## Description
+
+This project implements a custom `_printf` function that mimics the behavior of the standard `printf` function in C. It supports a variety of format specifiers, including both standard and custom ones, and handles various flags, field widths, and precision.
+
+## Compilation
+
+To compile the project, use the following command:
+
+```sh
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c
+
--- printf.c
+++ printf.c
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
 #include <ctype.h>
@@ -7,10 +8,17 @@
 
 #define BUFFER_SIZE 1024
 
-void rot13(char *str) {
+/**
+ * rot13 - Encodes a string using ROT13.
+ * @str: The string to be encoded.
+ */
+void rot13(char *str)
+{
     char *p = str;
-    while (*p) {
-        if (isalpha(*p)) {
+    while (*p)
+    {
+        if (isalpha(*p))
+        {
             char base = islower(*p) ? 'a' : 'A';
             *p = (char)(((*p - base + 13) % 26) + base);
         }
@@ -18,6 +26,12 @@ void rot13(char *str) {
     }
 }
 
+/**
+ * _printf - Custom printf function.
+ * @format: The format string.
+ * @...: The values to format and print.
+ * Return: The number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
     int i = 0, count = 0, buffer_index = 0;
@@ -25,12 +39,12 @@ int _printf(const char *format, ...)
     va_list args;
     char c;
     const char *s;
-    char rot13_str[100];
     unsigned long num;
     unsigned long ptr;
+    char num_buf[100], ptr_buf[100];
     int j, digit, field_width, precision, length_modifier;
     int zero_flag, minus_flag, plus_flag, space_flag, hash_flag;
-    
+
     if (!format)
         return (-1);
 
@@ -38,7 +52,6 @@ int _printf(const char *format, ...)
 
     while (format && format[i])
     {
-        // Reset flags and modifiers
         field_width = 0;
         precision = -1;
         length_modifier = 0;
@@ -48,7 +61,6 @@ int _printf(const char *format, ...)
         space_flag = 0;
         hash_flag = 0;
 
-        // Check for flags
         while (format[i] == '-' || format[i] == '0' || format[i] == '+' || format[i] == ' ' || format[i] == '#')
         {
             if (format[i] == '-')
@@ -64,14 +76,12 @@ int _printf(const char *format, ...)
             i++;
         }
 
-        // Check for field width
         while (format[i] >= '0' && format[i] <= '9')
         {
             field_width = field_width * 10 + (format[i] - '0');
             i++;
         }
 
-        // Check for precision
         if (format[i] == '.')
         {
             i++;
@@ -83,7 +93,6 @@ int _printf(const char *format, ...)
             }
         }
 
-        // Check for length modifiers
         while (format[i] == 'l' || format[i] == 'h')
         {
             if (format[i] == 'l')
@@ -109,6 +118,7 @@ int _printf(const char *format, ...)
                     buffer[buffer_index++] = c;
                     count++;
                     break;
+
                 case 's':
                     s = va_arg(args, const char *);
                     if (!s)
@@ -125,6 +135,7 @@ int _printf(const char *format, ...)
                         count++;
                     }
                     break;
+
                 case '%':
                     if (buffer_index >= BUFFER_SIZE - 1)
                     {
@@ -135,11 +146,11 @@ int _printf(const char *format, ...)
                     buffer[buffer_index++] = '%';
                     count++;
                     break;
+
                 case 'd':
                 case 'i':
                     num = va_arg(args, int);
-                    char num_buffer[100];
-                    int num_len = snprintf(num_buffer, sizeof(num_buffer), "%d", num);
+                    int num_len = snprintf(num_buf, sizeof(num_buf), "%d", num);
                     if (field_width > num_len)
                     {
                         int padding = field_width - num_len;
@@ -153,7 +164,7 @@ int _printf(const char *format, ...)
                                     count += buffer_index;
                                     buffer_index = 0;
                                 }
-                                buffer[buffer_index++] = num_buffer[j];
+                                buffer[buffer_index++] = num_buf[j];
                                 count++;
                             }
                             while (padding-- > 0)
@@ -187,7 +198,7 @@ int _printf(const char *format, ...)
                                     count += buffer_index;
                                     buffer_index = 0;
                                 }
-                                buffer[buffer_index++] = num_buffer[j];
+                                buffer[buffer_index++] = num_buf[j];
                                 count++;
                             }
                         }
@@ -202,11 +213,12 @@ int _printf(const char *format, ...)
                                 count += buffer_index;
                                 buffer_index = 0;
                             }
-                            buffer[buffer_index++] = num_buffer[j];
+                            buffer[buffer_index++] = num_buf[j];
                             count++;
                         }
                     }
                     break;
+
                 case 'u':
                 case 'o':
                 case 'x':
@@ -220,9 +232,6 @@ int _printf(const char *format, ...)
                     else
                         num = va_arg(args, unsigned int);
 
-                    // Convert number to string here (you may need custom conversion functions)
-                    // For simplicity, assuming snprintf or similar function for demonstration
-                    char num_buf[100];
                     int num_len = snprintf(num_buf, sizeof(num_buf), 
                         format[i] == 'x' ? "%x" : format[i] == 'X' ? "%X" : "%u", num);
                     if (field_width > num_len)
@@ -292,9 +301,9 @@ int _printf(const char *format, ...)
                         }
                     }
                     break;
+
                 case 'p':
                     ptr = va_arg(args, unsigned long);
-                    char ptr_buf[100];
                     int ptr_len = snprintf(ptr_buf, sizeof(ptr_buf), "0x%lx", ptr);
                     if (field_width > ptr_len)
                     {
@@ -363,6 +372,27 @@ int _printf(const char *format, ...)
                         }
                     }
                     break;
+
+                case 'R':
+                    s = va_arg(args, const char *);
+                    if (!s)
+                        s = "(null)";
+                    char rot13_buf[BUFFER_SIZE];
+                    char *p = rot13_buf;
+                    rot13(p);
+                    while (*p)
+                    {
+                        if (buffer_index >= BUFFER_SIZE - 1)
+                        {
+                            write(1, buffer, buffer_index);
+                            count += buffer_index;
+                            buffer_index = 0;
+                        }
+                        buffer[buffer_index++] = *p++;
+                        count++;
+                    }
+                    break;
+
                 case 'S':
                     s = va_arg(args, const char *);
                     if (!s)
@@ -383,94 +413,7 @@ int _printf(const char *format, ...)
                     }
                     count += buffer_index;
                     break;
-                case 'b':
-                    num = va_arg(args, unsigned int);
-                    char bin_buf[100];
-                    int bin_len = 0;
-                    if (num == 0)
-                        bin_buf[bin_len++] = '0';
-                    else
-                    {
-                        while (num)
-                        {
-                            bin_buf[bin_len++] = (num & 1) ? '1' : '0';
-                            num >>= 1;
-                        }
-                        for (int k = 0; k < bin_len / 2; k++)
-                        {
-                            char tmp = bin_buf[k];
-                            bin_buf[k] = bin_buf[bin_len - k - 1];
-                            bin_buf[bin_len - k - 1] = tmp;
-                        }
-                    }
-                    bin_buf[bin_len] = '\0';
-                    if (field_width > bin_len)
-                    {
-                        int padding = field_width - bin_len;
-                        if (minus_flag)
-                        {
-                            for (j = 0; j < bin_len; j++)
-                            {
-                                if (buffer_index >= BUFFER_SIZE - 1)
-                                {
-                                    write(1, buffer, buffer_index);
-                                    count += buffer_index;
-                                    buffer_index = 0;
-                                }
-                                buffer[buffer_index++] = bin_buf[j];
-                                count++;
-                            }
-                            while (padding-- > 0)
-                            {
-                                if (buffer_index >= BUFFER_SIZE - 1)
-                                {
-                                    write(1, buffer, buffer_index);
-                                    count += buffer_index;
-                                    buffer_index = 0;
-                                }
-                                buffer[buffer_index++] = ' ';
-                            }
-                        }
-                        else
-                        {
-                            while (padding-- > 0)
-                            {
-                                if (buffer_index >= BUFFER_SIZE - 1)
-                                {
-                                    write(1, buffer, buffer_index);
-                                    count += buffer_index;
-                                    buffer_index = 0;
-                                }
-                                buffer[buffer_index++] = zero_flag ? '0' : ' ';
-                            }
-                            for (j = 0; j < bin_len; j++)
-                            {
-                                if (buffer_index >= BUFFER_SIZE - 1)
-                                {
-                                    write(1, buffer, buffer_index);
-                                    count += buffer_index;
-                                    buffer_index = 0;
-                                }
-                                buffer[buffer_index++] = bin_buf[j];
-                                count++;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        for (j = 0; j < bin_len; j++)
-                        {
-                            if (buffer_index >= BUFFER_SIZE - 1)
-                            {
-                                write(1, buffer, buffer_index);
-                                count += buffer_index;
-                                buffer_index = 0;
-                            }
-                            buffer[buffer_index++] = bin_buf[j];
-                            count++;
-                        }
-                    }
-                    break;
+
                 default:
                     break;
             }
@@ -488,6 +431,7 @@ int _printf(const char *format, ...)
             count++;
         }
     }
+
     if (buffer_index > 0)
     {
         write(1, buffer, buffer_index);
